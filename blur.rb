2ab10d32bcb2b4819e350420846c7d5e3bacc43c
Chris Calendine <ccalendine@yahoo.com>
2019-10-31 06:02:21 +0000

    built blur1 class to output data only

--- blur.rb
+++ blur.rb
@@ -1,1 +1,21 @@
-puts "hello"
+class Image
+
+def initialize(pic)
+    @pic = pic
+end
+
+def output_image
+  @pic.each do |data|
+    puts data.join
+  end
+end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
